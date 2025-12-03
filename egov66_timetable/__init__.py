677ab8a4eb0fc8a8523e4d84ed8e8f6f3f5a8943
Матвей Вялков <vyalkov.2002@mail.ru>
2025-12-03 17:45:37 +0500

    Объединять ячейки в таблице, если несколько одинаковых пар идут подряд

--- egov66_timetable/__init__.py
+++ egov66_timetable/__init__.py
@@ -13,10 +13,13 @@ from pathlib import Path
 import jinja2
 
 from egov66_timetable.client import Client
-from egov66_timetable.types import Settings
+from egov66_timetable.types import Settings, Timetable
 from egov66_timetable.utils import get_current_week
 
 __version__ = "0.0.0"
+
+# classroom, name, rowspan
+type CollapsedTimetable = list[list[tuple[str, str, int]]]
 
 
 def load_template() -> jinja2.Template:
@@ -31,6 +34,48 @@ def load_template() -> jinja2.Template:
         lstrip_blocks=True,
     )
     return jinja_env.get_template("week.html.jinja")
+
+
+def collapse_timetable(timetable: Timetable) -> CollapsedTimetable:
+    """
+    Преобразует расписание в пригодное для рендеринга в формате таблицы, а
+    именно переводит его из формата ``{номер_пары: (аудитория, предмет)}`` в
+    формат ``(аудитория, предмет, число_повторов)``.
+
+    Если расписания на день нет, вставляет вместо него три пустые строки.
+
+    :param timetable: исходное расписание
+    """
+
+    result: CollapsedTimetable = [[] for _ in range(len(timetable))]
+    # pylint: disable=consider-using-enumerate
+    for day in range(len(timetable)):
+        if len(timetable[day]) != 0:
+            for lesson_num in range(max(timetable[day]) + 1):
+                this_lesson = timetable[day].get(lesson_num)
+                classroom, name = this_lesson or ["нет", ""]
+
+                if (
+                    # если это первая итерация
+                    len(result[day]) == 0
+                    # если вместо пары "окно"
+                    or this_lesson is None
+                    # если эта пара такая же, как прошлая
+                    or (classroom, name) != result[day][-1][:2]
+                ):
+                    result[day].append(
+                        (classroom, name, 1)
+                    )
+                else:
+                    # Увеличиваем rowspan на 1
+                    result[day][-1] = (
+                        *result[day][-1][:2], result[day][-1][2] + 1
+                    )
+        else:
+            # Расписания еще нет, добавляем 3 пустые строки.
+            result[day].extend([("", "", 1)] * 3)
+
+    return result
 
 
 def write_timetable(group: str, *, settings: Settings, offset: int = 0,
@@ -62,7 +107,7 @@ def write_timetable(group: str, *, settings: Settings, offset: int = 0,
     html = template.render(
         css_path=settings.get("css_path", "../egov66_timetable/static/styles.css"),
         group=group,
-        timetable=timetable,
+        timetable=collapse_timetable(timetable),
         week=week,
         timedelta=timedelta,
         **template_args,
---
+++ tests/test_main.py
@@ -0,0 +1,73 @@
+# SPDX-License-Identifier: WTFPL
+# SPDX-FileCopyrightText: 2025 Matvey Vyalkov
+# No warranty
+
+from egov66_timetable import collapse_timetable
+from egov66_timetable.types import Timetable
+
+
+def test_collapse_timetable():
+    day = {
+        0: ("100", "А"),
+        1: ("100", "А"),
+        2: ("200", "Б"),
+        3: ("100", "А"),
+        4: ("100", "А"),
+    }
+    timetable: Timetable = [day.copy() for _ in range(7)]
+
+    day_collapsed = [
+        ("100", "А", 2),
+        ("200", "Б", 1),
+        ("100", "А", 2),
+    ]
+    expected = [day_collapsed.copy() for _ in range(7)]
+
+    assert collapse_timetable(timetable) == expected
+
+
+def test_collapse_timetable_with_space_in_the_middle():
+    day = {
+        0: ("100", "А"),
+        2: ("200", "Б"),
+        3: ("300", "В"),
+    }
+    timetable: Timetable = [day.copy() for _ in range(7)]
+
+    day_collapsed = [
+        ("100", "А", 1),
+        ("нет", "", 1),
+        ("200", "Б", 1),
+        ("300", "В", 1),
+    ]
+    expected = [day_collapsed.copy() for _ in range(7)]
+
+    assert collapse_timetable(timetable) == expected
+
+
+def test_collapse_timetable_with_space_in_the_beginning():
+    day = {
+        1: ("100", "А"),
+        2: ("200", "Б"),
+        3: ("300", "В"),
+    }
+    timetable: Timetable = [day.copy() for _ in range(7)]
+
+    day_collapsed = [
+        ("нет", "", 1),
+        ("100", "А", 1),
+        ("200", "Б", 1),
+        ("300", "В", 1),
+    ]
+    expected = [day_collapsed.copy() for _ in range(7)]
+
+    assert collapse_timetable(timetable) == expected
+
+
+def test_collapse_timetable_empty():
+    timetable: Timetable = [{} for _ in range(7)]
+
+    day_collapsed = [("", "", 1)] * 3
+    expected = [day_collapsed.copy() for _ in range(7)]
+
+    assert collapse_timetable(timetable) == expected
